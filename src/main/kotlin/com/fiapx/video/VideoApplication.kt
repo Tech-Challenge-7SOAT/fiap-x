33e016d90a34c2861b714b38e38a0dd9f3d8c710
Rafael Acioly <aciolyr@gmail.com>
2025-01-27 20:46:01 -0300

    add swagger page

--- src/main/kotlin/com/fiapx/video/VideoApplication.kt
+++ src/main/kotlin/com/fiapx/video/VideoApplication.kt
@@ -1,9 +1,12 @@
 package com.fiapx.video
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(info = Info(title = "FiapX - video processor", version = "1.0.0"))
 class VideoApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/fiapx/video/http/controllers/VideoController.kt
+++ src/main/kotlin/com/fiapx/video/http/controllers/VideoController.kt
@@ -1,18 +1,39 @@
 package com.fiapx.video.http.controllers
 
 import com.fiapx.video.services.VideoService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
+@Tag(name = "Video", description = "Video controller")
 class VideoController(private val service: VideoService) {
 
     @GetMapping("/")
+    @Operation(summary = "Get videos")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "OK"),
+        ApiResponse(responseCode = "500", description = "Internal Server Error")
+    ])
     fun index() = service.findAll()
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get video by id")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "OK"),
+        ApiResponse(responseCode = "404", description = "Not Found"),
+        ApiResponse(responseCode = "500", description = "Internal Server Error")
+    ])
     fun find(@PathVariable id: Long) = service.findById(id)
 
     @PostMapping("/")
+    @Operation(summary = "Store videos")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "OK"),
+        ApiResponse(responseCode = "500", description = "Internal Server Error")
+    ])
     fun store(@RequestParam("videos") videos: List<MultipartFile>) = service.store(videos)
 }
